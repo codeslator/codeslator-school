ff2a7a415118f3a51967e696afe0eb7b93200d92
Andres Melendez <codeslator@gmail.com>
2022-05-03 15:01:13 -0400

    Adding react stroll to smooth navigation

--- src/views/layouts/website/NavBar.tsx
+++ src/views/layouts/website/NavBar.tsx
@@ -1,24 +1,29 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
 import { Box, Button } from '@mui/material'
 import { NavLink } from "react-router-dom"
+import { Link } from 'react-scroll'
 import { sectionNavigation } from './navigation';
 import { authNavigation } from '../auth/navigation';
 
 const NavBar: FC = () => {
+  const [match, setMatch] = useState('');
+
+  // useEffect(() => setMatch(match), [match])
+  
   return (
     <>
       <Box>
         {sectionNavigation.map(({ name, to }) => (
-          <Button
-            component={NavLink}
-            to={to}
-            key={to}
-            variant="outlined"
-            color="inherit"
-            sx={{ mx: .5 }}
-          >
-            {name}
-          </Button>
+          <Link key={to} to={to} smooth spy duration={500} offset={-60} onSetActive={(route) => setMatch(route)}>
+            <Button
+              // href={to}
+              variant={(match === to) ? 'contained' : 'outlined'}
+              color={(match === to) ? 'secondary' : 'inherit'}
+              sx={{ mx: .5 }}
+            >
+              {name}
+            </Button>
+          </Link>
         ))}
       </Box>
       <Box>
